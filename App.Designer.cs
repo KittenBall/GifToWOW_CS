1b0f2d1367b75a178bd782eaa9dc51a73fc47391
GreenhairTurtle <912967642@qq.com>
2022-02-17 18:06:32 +0800

    update

--- App.Designer.cs
+++ App.Designer.cs
@@ -28,21 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.SelectGifButton = new Sunny.UI.UIButton();
+            this.SelectOptDirButton = new Sunny.UI.UIButton();
+            this.OptDirPathLabel = new Sunny.UI.UILabel();
+            this.GifFrames = new System.Windows.Forms.FlowLayoutPanel();
+            this.GifInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // SelectGifButton
+            // 
+            this.SelectGifButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectGifButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SelectGifButton.FillColor = System.Drawing.Color.White;
+            this.SelectGifButton.FillColor2 = System.Drawing.Color.White;
+            this.SelectGifButton.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectGifButton.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectGifButton.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectGifButton.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SelectGifButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(98)))), ((int)(((byte)(102)))));
+            this.SelectGifButton.ForeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.SelectGifButton.ForePressColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(131)))), ((int)(((byte)(229)))));
+            this.SelectGifButton.ForeSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(131)))), ((int)(((byte)(229)))));
+            this.SelectGifButton.Location = new System.Drawing.Point(688, 452);
+            this.SelectGifButton.MinimumSize = new System.Drawing.Size(1, 1);
+            this.SelectGifButton.Name = "SelectGifButton";
+            this.SelectGifButton.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.SelectGifButton.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(222)))), ((int)(((byte)(255)))));
+            this.SelectGifButton.Size = new System.Drawing.Size(100, 35);
+            this.SelectGifButton.Style = Sunny.UI.UIStyle.Custom;
+            this.SelectGifButton.TabIndex = 0;
+            this.SelectGifButton.Text = "选择Gif";
+            this.SelectGifButton.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SelectGifButton.Click += new System.EventHandler(this.SelectGifButton_Click);
+            // 
+            // SelectOptDirButton
+            // 
+            this.SelectOptDirButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectOptDirButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SelectOptDirButton.FillColor = System.Drawing.Color.White;
+            this.SelectOptDirButton.FillColor2 = System.Drawing.Color.White;
+            this.SelectOptDirButton.FillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectOptDirButton.FillPressColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectOptDirButton.FillSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.SelectOptDirButton.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SelectOptDirButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(98)))), ((int)(((byte)(102)))));
+            this.SelectOptDirButton.ForeHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.SelectOptDirButton.ForePressColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(131)))), ((int)(((byte)(229)))));
+            this.SelectOptDirButton.ForeSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(131)))), ((int)(((byte)(229)))));
+            this.SelectOptDirButton.Location = new System.Drawing.Point(541, 452);
+            this.SelectOptDirButton.MinimumSize = new System.Drawing.Size(1, 1);
+            this.SelectOptDirButton.Name = "SelectOptDirButton";
+            this.SelectOptDirButton.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.SelectOptDirButton.RectHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(222)))), ((int)(((byte)(255)))));
+            this.SelectOptDirButton.Size = new System.Drawing.Size(141, 35);
+            this.SelectOptDirButton.Style = Sunny.UI.UIStyle.Custom;
+            this.SelectOptDirButton.TabIndex = 1;
+            this.SelectOptDirButton.Text = "选择输出文件夹";
+            this.SelectOptDirButton.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SelectOptDirButton.Click += new System.EventHandler(this.SelectOptDirButton_Click);
+            // 
+            // OptDirPathLabel
+            // 
+            this.OptDirPathLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OptDirPathLabel.AutoEllipsis = true;
+            this.OptDirPathLabel.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.OptDirPathLabel.Location = new System.Drawing.Point(18, 459);
+            this.OptDirPathLabel.Name = "OptDirPathLabel";
+            this.OptDirPathLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.OptDirPathLabel.Size = new System.Drawing.Size(517, 28);
+            this.OptDirPathLabel.Style = Sunny.UI.UIStyle.Custom;
+            this.OptDirPathLabel.TabIndex = 2;
+            this.OptDirPathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // GifFrames
+            // 
+            this.GifFrames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GifFrames.AutoScroll = true;
+            this.GifFrames.Location = new System.Drawing.Point(3, 38);
+            this.GifFrames.Name = "GifFrames";
+            this.GifFrames.Size = new System.Drawing.Size(794, 353);
+            this.GifFrames.TabIndex = 3;
+            // 
+            // GifInfo
+            // 
+            this.GifInfo.AutoSize = true;
+            this.GifInfo.Location = new System.Drawing.Point(3, 407);
+            this.GifInfo.Name = "GifInfo";
+            this.GifInfo.Size = new System.Drawing.Size(55, 21);
+            this.GifInfo.TabIndex = 4;
+            this.GifInfo.Text = "label1";
             // 
             // App
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 500);
+            this.ControlBoxFillHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.ControlBoxForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(98)))), ((int)(((byte)(102)))));
+            this.Controls.Add(this.GifInfo);
+            this.Controls.Add(this.GifFrames);
+            this.Controls.Add(this.OptDirPathLabel);
+            this.Controls.Add(this.SelectOptDirButton);
+            this.Controls.Add(this.SelectGifButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "App";
+            this.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.ShowRect = false;
             this.ShowTitleIcon = true;
+            this.Style = Sunny.UI.UIStyle.Custom;
             this.Text = "GifConverter";
+            this.TitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(227)))));
+            this.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(98)))), ((int)(((byte)(102)))));
+            this.SizeChanged += new System.EventHandler(this.App_SizeChanged);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Sunny.UI.UIButton SelectGifButton;
+        private Sunny.UI.UIButton SelectOptDirButton;
+        private Sunny.UI.UILabel OptDirPathLabel;
+        private System.Windows.Forms.FlowLayoutPanel GifFrames;
+        private System.Windows.Forms.Label GifInfo;
     }
 }
 
--- App.cs
+++ App.cs
@@ -3,19 +3,239 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Sunny.UI;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 
 namespace GifToWOW
 {
-    public partial class App : UIForm
+    public partial class App : UIForm, GifConverter.IGifConverterListener
     {
+        private string gifPath;
+        private string optDirPath;
+        private GifConverter gifConverter;
+        private readonly string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private List<PictureBox> pictureBoxes;
+
         public App()
         {
+            gifConverter = new GifConverter(this);
             InitializeComponent();
+            OptDirPathLabel.Text = desktopPath;
+            pictureBoxes = new List<PictureBox>();
+        }
+
+        private void SelectGifButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog gifSelectDialog = new OpenFileDialog();
+            gifSelectDialog.Filter = "Gif文件|*.gif";
+            gifSelectDialog.Title = "选择Gif文件";
+            gifSelectDialog.InitialDirectory = gifPath == null ? desktopPath : Path.GetDirectoryName(gifPath);
+            if (gifSelectDialog.ShowDialog() == DialogResult.OK)
+            {
+                gifPath = gifSelectDialog.FileName;
+                OnGifSelected();
+            }
+        }
+
+        private void SelectOptDirButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialogEx optDirDialog = new FolderBrowserDialogEx();
+            optDirDialog.Description = "选择输出文件夹";
+            optDirDialog.DirectoryPath = optDirPath == null ? desktopPath : optDirPath;
+            DialogResult dialogResult = optDirDialog.ShowDialog(this);
+            if (dialogResult == DialogResult.OK) 
+            {
+                optDirPath = optDirDialog.DirectoryPath;
+                OptDirPathLabel.Text = optDirPath;
+            }
+        }
+
+        private void GifFrames_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void App_SizeChanged(object sender, EventArgs e)
+        {
+            GifFrames.Height = (int)(this.Height * 0.65);
+        }
+
+        private void OnGifSelected()
+        {
+            GifFrames.Controls.Clear();
+            gifConverter.SetGifPath(gifPath);
+        }
+
+        private void DisablePictureBoxAnimate(PictureBox pictureBox)
+        {
+            var animateMethod = typeof(PictureBox).GetMethod("Animate",
+            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
+            null, new Type[] { typeof(bool) }, null);
+            animateMethod.Invoke(pictureBox, new object[] { false });
+        }
+
+        public void OnProcessStart()
+        {
+            
+        }
+
+        public void OnProcessEnd()
+        {
+            var frames = gifConverter.GetFrames();
+            var count = frames.Count;
+            for (int i = 0; i < count; i++)
+            {
+                PictureBox picture = null;
+                if (i < pictureBoxes.Count) 
+                {
+                    picture = pictureBoxes[i];
+                }
+                else
+                {
+                    picture = new PictureBox();
+                    picture.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBoxes.Add(picture);
+                }
+
+                picture.Size = new Size((int)(GifFrames.Width / 5.5), (int)(GifFrames.Width / 5.5));
+
+                picture.Image = new Bitmap(frames[i]);
+
+                GifFrames.Controls.Add(picture);
+
+            }
+            Console.WriteLine(gifConverter.GetDuration());
+        }
+
+        public void OnConvertStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConvertEnd()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnConvertError()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnProcessError(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        public void OnConvertError(Exception ex)
+        {
+            
+        }
+    }
+
+    public class GifConverter
+    {
+
+        private readonly IGifConverterListener listener;
+        private string gifPath;
+        private Image gifImage;
+        private List<Image> gifFrames = new List<Image>();
+
+        public GifConverter(IGifConverterListener listener)
+        {
+            this.listener = listener;
+        }
+
+        public void SetGifPath(string gifPath)
+        {
+            this.gifPath = gifPath;
+            ProcessGif();
+        }
+
+        private  Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void ProcessGif()
+        {
+            listener.OnProcessStart();
+            try
+            {
+                gifImage = null;
+                gifFrames.Clear();
+                if (gifPath == null || !File.Exists(gifPath))
+                { 
+                    listener.OnProcessError(new Exception("Gif不存在"));
+                    return;
+                }
+
+                gifImage = Image.FromFile(gifPath);
+                int frameCount = gifImage.GetFrameCount(FrameDimension.Time);
+                for (int i = 0; i < frameCount; i++) {
+                    gifImage.SelectActiveFrame(FrameDimension.Time, i);
+                    gifFrames.Add(gifImage.Clone() as Image);
+                }
+
+                listener.OnProcessEnd();
+            }
+            catch (Exception ex)
+            {
+                listener.OnProcessError(ex);
+            }
+        }
+
+        public List<Image> GetFrames()
+        { 
+            return gifFrames;
+        }
+
+        public int GetDuration()
+        {
+            PropertyItem item = gifImage.GetPropertyItem(0x5100); // FrameDelay in libgdiplus
+                                                             // Time is in milliseconds
+            return (item.Value[0] + item.Value[1] * 256) * 10;
+        }
+
+        public interface IGifConverterListener 
+        {
+            void OnProcessStart();
+
+            void OnProcessEnd();
+
+            void OnProcessError(Exception ex);
+
+            void OnConvertStart();
+
+            void OnConvertEnd();
+
+            void OnConvertError(Exception ex);
         }
     }
 }
